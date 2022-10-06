9a7dc687407e8bd79dc99b7f1d067bc2adec0424
Gusta <gustavo_a_pereira@estudante.sc.senai.br>
2022-10-06 10:43:23 -0300

    commit

--- inserir.php
+++ inserir.php
@@ -3,10 +3,17 @@
 
     $email = $_POST["email"];
     $senha = MD5($_POST["senha"]);
-    $comando = $pdo -> prepare("INSERT INTO usuario (email_usuario, senha_usuario) VALUES(:email,:senha)");
+    $nome = $_POST["nome"];
+    $cpf = $_POST["cpf"];
+    $datansc = $_POST["datansc"];
+    $comando = $pdo -> prepare("INSERT INTO usuario (email_usuario, senha_usuario, nome_usuario, cpf, data_nascimento) VALUES(:email,:senha,:nome,:cpf,:datansc)");
     $comando->bindValue(":email",$email);                                     
-    $comando->bindValue(":senha",$senha);    
+    $comando->bindValue(":senha",$senha);
+    $comando->bindValue(":nome",$nome);                                     
+    $comando->bindValue(":cpf",$cpf);
+    $comando->bindValue(":datansc",$datansc);                             
+
     $comando->execute();                               
 
-    header("Location:cadastrar.php");
+    header("Location:catalogo.html");
 ?>
--- login.php
+++ login.php
@@ -4,16 +4,22 @@ include("conexao.php");
 $email =$_POST["email"];
 $set_senha =$_POST["senha"];
 
-$comando = $pdo->prepare("SELECT cod_usuario, senha_usuario, login_usuario, perm_admin FROM usuario WHERE email_usuario = :email");
+$comando = $pdo->prepare("SELECT cod_usuario, senha_usuario, perm_admin FROM usuario WHERE email_usuario = :email");
 
 $comando->bindValue(":email",$email);
 $comando->execute();
 
-if($comando->rowCount() == 1){
+if($comando->rowCount()==1)
+{
     $resultado = $comando->fetch();
-
-    if($resultado['senha_usuario'] == MD5($set_senha)){
-       header("Location:pageCadFilme.php");
+    if($resultado['senha_usuario']== MD5($set_senha)){
+        if ($resultado['perm_admin'] == 1) {
+            header("Location:pageCadFilme.php");
+        } else {
+            header("Location:catalogo.html");
+        }
+            
+            
 
        session_start();
        $_SESSION['cod_usuario'] = $resultado['cod_usuario'];
@@ -21,10 +27,14 @@ if($comando->rowCount() == 1){
        $_SESSION['loggedin'] = true;
 
     }else{
-        echo("Email ou senha incorreto");
+        echo("Email incorreto!");
+        echo($email);
+        echo($set_senha);
+        echo(MD5($set_senha));
+
     }
 }else{
- echo("Email ou senha incorreto");
+ echo("Senha incorreta!");
 
 }
 

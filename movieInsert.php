944ed97a449c08c12372a3366ed24e27e57c5e34
Gusta <gustavo_a_pereira@estudante.sc.senai.br>
2022-10-10 12:03:24 -0300

    commit

--- movieInsert.php
+++ movieInsert.php
@@ -1,11 +1,6 @@
 <?php
     include("conexao.php");
 
-    session_start();
-    $usuario = $_SESSION['cod_usuario'];
-
-    $puff = $_COOKIE["cadeiras"];
-    $valortotal = $_COOKIE["valor"];
     $filme = $_POST["filme"];
     $comando = $pdo -> prepare("INSERT INTO ingresso (puff_ingresso, filme_sessao, total_ingresso, fk_usuario) VALUES(:puff,:filme,:total,:usuario)");
     $comando->bindValue(":puff",$puff); 
